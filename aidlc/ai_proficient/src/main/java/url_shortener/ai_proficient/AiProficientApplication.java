caf93f6aeb995dff822c671e4af8d00e7dc138b3
SowdnaryaTummalapalli <tummalapalli.sowndarya123@gmail.com>
2026-07-30 12:27:21 +0530

    created basic java springboot application structure

---
+++ aidlc/ai_proficient/src/main/java/url_shortener/ai_proficient/AiProficientApplication.java
@@ -0,0 +1,13 @@
+package url_shortener.ai_proficient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProficientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProficientApplication.class, args);
+	}
+
+}
---
+++ aidlc/ai_proficient/src/test/java/url_shortener/ai_proficient/AiProficientApplicationTests.java
@@ -0,0 +1,13 @@
+package url_shortener.ai_proficient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProficientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
